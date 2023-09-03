de95fbeb0c4b84fd0208dcb5da77d09dcb8b9831
BachininaAnna <125889059+BachininaAnna@users.noreply.github.com>
2023-09-04 00:05:48 +0300

    test complete

--- scripts/test.js
+++ scripts/test.js
@@ -1,9 +1,14 @@
 /*(function () {*/
 const Test = {
+    progressBarElement: null,
+    nextButtonElement: null,
+    prevButtonElement: null,
+    passButtonElement: null,
     questionTitleElement: null,
     optionsElement: null,
     quiz: null,
     currentQuestionIndex: 1,
+    userResult: [],
     init() {
         checkUserData();
         const url = new URL(location.href);
@@ -28,11 +33,53 @@ const Test = {
     },
     startQuiz() {
         console.log(this.quiz);
+
+        document.getElementById('pre-title').innerText = this.quiz.name;
         this.questionTitleElement = document.getElementById('title');
         this.optionsElement = document.getElementById('options');
+        this.progressBarElement = document.getElementById('progress-bar');
 
+        this.nextButtonElement = document.getElementById('next');
+        this.nextButtonElement.onclick = this.move.bind(this, 'next');/* -----------вот это не поняла совсем------------- */
 
+        this.prevButtonElement = document.getElementById('prev');
+        this.prevButtonElement.onclick = this.move.bind(this, 'prev');/* -----------вот это не поняла совсем------------- */
+
+        this.passButtonElement = document.getElementById('pass');
+        this.passButtonElement.onclick = this.move.bind(this, 'pass');/* -----------вот это не поняла совсем------------- */
+
+        this.prepareProgressBar();
         this.showQuestion();
+
+        const timerElement = document.getElementById('timer');
+        let seconds = 59;
+        const interval = setInterval(function () {
+            seconds--;
+            timerElement.innerText = seconds;
+
+            if (seconds === 0) {
+                clearInterval(interval);
+                this.complete();
+            }
+        }.bind(this), 1000);
+    },
+    prepareProgressBar() {
+        for (let i = 0; i < this.quiz.questions.length; i++) {
+            const itemElement = document.createElement('div');
+            itemElement.className = 'test-progress-bar-item' + (i === 0 ? ' active' : '');
+
+            const itemCircleElement = document.createElement('div');
+            itemCircleElement.className = 'test-progress-bar-item-circle';
+
+            const itemTextElement = document.createElement('div');
+            itemTextElement.className = 'test-progress-bar-item-text';
+            itemTextElement.innerText = 'Вопрос ' + (i + 1);
+
+            itemElement.appendChild(itemCircleElement);
+            itemElement.appendChild(itemTextElement);
+
+            this.progressBarElement.appendChild(itemElement);
+        }
     },
     showQuestion() {
         const activeQuestion = this.quiz.questions[this.currentQuestionIndex - 1];
@@ -40,16 +87,27 @@ const Test = {
             + ':</span> ' + activeQuestion.question;
 
         this.optionsElement.innerHTML = '';
+        const that = this;
+        const chosenOption = this.userResult.find(item => item.questionId === activeQuestion.id);
+
         activeQuestion.answers.forEach(answer => {
             const optionElement = document.createElement('div');
             optionElement.className = 'test-question-option';
 
             const inputId = 'answer-' + answer.id;
             const inputElement = document.createElement('input');
+            inputElement.className = 'option-answer';
             inputElement.setAttribute('id', inputId);
             inputElement.setAttribute('type', 'radio');
             inputElement.setAttribute('name', 'answer');
             inputElement.setAttribute('value', answer.id);
+            if (chosenOption && chosenOption.chosenAnswerId === answer.id) {
+                inputElement.setAttribute('checked', 'checked');
+            }
+
+            inputElement.onchange = function () {
+                that.chooseAnswer();
+            }
 
             const labelElement = document.createElement('label');
             labelElement.setAttribute('for', inputId);
@@ -60,6 +118,77 @@ const Test = {
 
             this.optionsElement.appendChild(optionElement);
         })
+
+
+        if (chosenOption && chosenOption.chosenAnswerId) { /*проверяем есть ли отвеченный вопрос, если уже есть. то кнопку не дизэйблим*/
+            this.nextButtonElement.removeAttribute('disabled');
+        } else {
+            this.nextButtonElement.setAttribute('disabled', 'disabled');
+        }
+
+
+        if (this.currentQuestionIndex === this.quiz.questions.length) {
+            this.nextButtonElement.innerText = 'Завершить';
+        } else {
+            this.nextButtonElement.innerText = 'Далее';
+        }
+        if (this.currentQuestionIndex > 1) {
+            this.prevButtonElement.removeAttribute('disabled');
+        } else {
+            this.prevButtonElement.setAttribute('disabled', 'disabled');
+        }
+    },
+    chooseAnswer() {
+        this.nextButtonElement.removeAttribute('disabled');
+    },
+    move(action) {
+        const activeQuestion = this.quiz.questions[this.currentQuestionIndex - 1];
+        const chosenAnswer = Array.from(document.getElementsByClassName('option-answer')).find(element => {/*Array.from() превращаем коллекцию в массив*/
+            return element.checked;
+        });
+
+        let chosenAnswerId = null;
+        if (chosenAnswer && chosenAnswer.value) {
+            chosenAnswerId = Number(chosenAnswer.value);
+        }
+        const existingResult = this.userResult.find(item => {
+            return item.questionId === activeQuestion.id
+        });
+
+        if (existingResult) {
+            existingResult.chosenAnswerId = chosenAnswerId;
+        } else {
+            this.userResult.push({
+                questionId: activeQuestion.id,
+                chosenAnswerId: chosenAnswerId
+            })
+        }
+
+        console.log(this.userResult);
+
+        if (action === 'pass' || action === 'next') {
+            this.currentQuestionIndex++;
+        } else {
+            this.currentQuestionIndex--;
+        }
+
+        Array.from(this.progressBarElement.children).forEach((item, index) => {
+            const currentItemIndex = index + 1;
+            item.classList.remove('complete');
+            item.classList.remove('active');
+
+            if (currentItemIndex === this.currentQuestionIndex) {
+                item.classList.add('active');
+            } else if (currentItemIndex < this.currentQuestionIndex) {
+                item.classList.add('complete');
+            }
+
+        });
+
+        this.showQuestion();
+    },
+    complete() {
+
     }
 
 }
